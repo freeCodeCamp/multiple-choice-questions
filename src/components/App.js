557bcb6922da75ed9595476c33a95a1216ebc5e5
Sean Smith <sean.smith.2009@gmail.com>
2017-05-27 19:45:46 -0700

    Adds shuffle quiz feature

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,29 @@ import quizzes from '../challenges';
 import Review from './Review';
 import Practice from './Practice';
 
+const shuffle = (array) => {
+	const cached = {};
+	const max = array.length - 1;
+	const randomize = () => Math.floor(Math.random() * (max + 1));
+	const generateIndex = () => {
+			let index = randomize();
+			while (index in cached) {
+					index = randomize();
+			};
+			cached[index] = true;
+			return index;
+	};
+	return array.reduce((shuffled, element) => {
+		const index = generateIndex();
+		shuffled[index] = Object.assign({}, element);
+		return shuffled;
+	}, []);
+};
+
+const findQuiz = (selected, quizzes) => {
+	return quizzes.filter(quiz => quiz.title === selected)[0];
+};
+
 export default class extends React.Component {
 	constructor(props) {
 		super(props);
@@ -16,17 +39,23 @@ export default class extends React.Component {
 	}
 	selectQuiz = (e) => {
 		const title = e.target.value;
-		const length = this.state.quizzes.filter(quiz => quiz.title === title )[0].challenges.length;
+		const length = this.state.quizzes.filter(quiz => quiz.title === title)[0].challenges.length;
 		this.setState({ selectedQuiz: title, length });
 	}
+	shuffleQuiz = () => {
+		const { selectedQuiz, quizzes } = this.state;
+		const quiz = findQuiz(selectedQuiz, quizzes);
+		quiz.challenges = shuffle(quiz.challenges);
+		this.setState({ quiz: quiz, session: 'practice' });
+	}
 	triggerPractice = () => {
-		const { selectedQuiz } = this.state;
-		const quiz = this.state.quizzes.filter(quiz => quiz.title === selectedQuiz )[0];
+		const { selectedQuiz, quizzes } = this.state;
+		const quiz = findQuiz(selectedQuiz, quizzes);
 		this.setState({ quiz: quiz, session: 'practice' });
 	}
 	triggerReview = () => {
-		const { selectedQuiz } = this.state;
-		const quiz = this.state.quizzes.filter(quiz => quiz.title === selectedQuiz )[0];
+		const { selectedQuiz, quizzes } = this.state;
+		const quiz = findQuiz(selectedQuiz, quizzes);
 		this.setState({ quiz: quiz, session: 'review' });
 	}
 	close = () => this.setState({ session: null })
@@ -58,7 +87,8 @@ export default class extends React.Component {
 				</select>
 				<p className='quizInfo'>This quiz has a total of {this.state.length} questions</p>
 				<button className='studyBtn' onClick={this.triggerPractice}>Practice Quiz</button>
-				<button className='reviewBtn' onClick={this.triggerReview}>View Questions</button>
+				<button className='shuffleBtn' onClick={this.shuffleQuiz}>Shuffle Questions</button>
+				<button className='reviewBtn' onClick={this.triggerReview}>Review Questions</button>
 				<div>
 					<h4>
 						<a
